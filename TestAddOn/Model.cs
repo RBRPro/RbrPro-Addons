b3177febbbb7beb2c249b11476adeecee186a564
TGD <carlo.iovino@outlook.com>
2020-08-14 22:16:35 +0200

    framework updated

--- TestAddOn/Model.cs
+++ TestAddOn/Model.cs
@@ -6,11 +6,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TGD.Framework;
+using TGD.Utils;
 
 namespace RBRProTestAddOn
 {
     public class Model : DynamicContextManager
     {
+        static IniFile _otherIni = new IniFile("OtherIniFile.ini");
+
         /// <summary>
         /// The Persistent Properties are loaded and saved automatically. They can have a default value. 
         /// Only C# base types only (the framework cannot persist complex types)
@@ -18,17 +21,32 @@ namespace RBRProTestAddOn
         #region PERSISTENT PROPERTIES
         [DefaultValue(true)]
         [ConfigProperty]
-        public bool ShowCarSpeed { get { return GetCfgProperty<bool>(); } set { SetCfgProperty(value); } }
+        public bool ShowCarSpeed { get { return GetIniProperty<bool>(); } set { SetIniProperty(value); } }
 
+        /// <summary>
+        /// This property is mapped in a specific section of the .ini File
+        /// </summary>
+        [ConfigProperty("Section2")] 
+        public string AnotherPersistentProperty { get { return GetIniProperty<string>(); } set { SetIniProperty(value); } }
+
+        /// <summary>
+        /// This property is mapped in a specific section of the .ini File with a specific name
+        /// </summary>
+        [ConfigProperty("Section2", "PropertyName")]
+        public string YetAnotherPersistentProperty { get { return GetIniProperty<string>(); } set { SetIniProperty(value); } }
+
+        /// <summary>
+        /// This property is mapped in a specific section of the .ini File with a specific name
+        /// </summary>
         [ConfigProperty]
-        public string AnotherPersistentProperty { get { return GetCfgProperty<string>(); } set { SetCfgProperty(value); } }
+        public string AdditionalPersistentProperty { get { return GetIniProperty<string>(_otherIni); } set { SetIniProperty(_otherIni, value); } }
         #endregion
 
         /// <summary>
         /// The Runtime Properties are the volatile ones. No limitations about the types: complex types and collections are allowed
         /// </summary>
         #region RUNTIME PROPERTIES
-        [RuntimeProperty]
+        [RuntimeProperty("Car.Speed")]
         public float CarSpeed { get { return GetRuntimeProperty<float>(); } set { SetRuntimeProperty(value); } }
 
         [RuntimeProperty]
